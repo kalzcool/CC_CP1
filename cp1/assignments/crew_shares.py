dd55948936d6d0da4bb26d303a091ac95c9631ff
kalzcool <nani.carrillo@ucas-edu.net>
2026-09-22 09:35:57 -0600

    crew shares 3rd attempt

--- cp1/assignments/crew_shares.py
+++ cp1/assignments/crew_shares.py
@@ -12,13 +12,13 @@ while True:
  
 num_units= random.randint(500, 5000)
 left_units= num_units -((num_pirates-2)*3)
-yondus_share=round( left_units*.13 +3, 2) 
-peters_share=round( (left_units- yondus_share)*.11 +3, 2)
+yondus_share=round( left_units*.13 , 2) 
+peters_share=round( (left_units- yondus_share)*.11, 2)
 left_total= left_units -(yondus_share+peters_share)
 crew_share= round(left_total/ num_pirates, 2 )
 
 print(f"Pirates = {num_pirates}")
 print(f"units = {num_units}")
-print(f"Yondu's share = {yondus_share}")
-print(f"peter's share = {peters_share}")
-print(f"crew share = {crew_share}")
+print(f"Yondu's share = {yondus_share: .2f}")
+print(f"peter's share = {peters_share:.2f}")
+print(f"crew share = {crew_share: .2f}")
