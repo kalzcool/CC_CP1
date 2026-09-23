69b501a577f0db4f3b5dc4c2a9af27fe2c2c0177
kalzcool <nani.carrillo@ucas-edu.net>
2026-09-23 09:46:37 -0600

    boolean notes

---
+++ cp1/notes/boolean_notes.py
@@ -0,0 +1,10 @@
+#CC boolean notes
+
+win = False
+
+admin= True
+age= -18 # only number that will not be true is 0, all strings are true unless its empy quotation marks.
+
+if 18 < age: #boolean equasion, always resulting in true or false. used in control structures to make it do different things 
+    print("You are an adult ")
+print(bool(age))
